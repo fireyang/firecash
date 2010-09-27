c9f8b942c741e6c01ff2709563418b8abbf45c06
FireYang <shenhuan007@gmail.com>
2010-09-27 18:58:01 +0800

    init project

---
+++ system/application/controllers/blog (fireyang-home's conflicted copy 2010-09-27).php
@@ -0,0 +1,12 @@
+<?php
+class Blog extends Controller{
+    function index()
+    {
+        echo 'Hello World,fireyang!';
+
+    }
+    function comments()
+    {
+        echo 'Lock at this!';
+    }
+}
---
+++ system/application/controllers/blog.php
@@ -0,0 +1,11 @@
+<?php
+class Blog extends Controller{
+    function index()
+    {
+        echo 'Hello World!';
+    }
+    function comments()
+    {
+        echo 'Lock at this!';
+    }
+}
