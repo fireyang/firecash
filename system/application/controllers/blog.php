7645ed1fd62da2932a24ff0e83e44befd7f0d26f
FireYang <shenhuan007@gmail.com>
2010-10-23 10:35:32 +0800

    update

--- system/application/controllers/blog.php
+++ system/application/controllers/blog.php
@@ -3,6 +3,9 @@ class Blog extends Controller{
     function index()
     {
         echo 'Hello World!';
+        $this->load->model('User_model');
+        $data = $this->User_model->get_last_ten_entries();
+        $this->load->view('blog',$data);
     }
     function comments()
     {
